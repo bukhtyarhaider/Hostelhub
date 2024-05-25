4e609f2897f3acc966c617ae7a8b0527471ae322
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-05-25 11:16:02 -0700

    empty App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,5 @@
-import { useState } from "react";
-import "./main.scss";
-import CustomInput from "./components/CustomInput/CustomInput";
-
 function App() {
-  const [value, setValue] = useState<string>("");
-  return (
-    <>
-      <CustomInput
-        placeholder="Email"
-        label="Email"
-        type="text"
-        value={value}
-        setValue={setValue}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
