cd83037dab67a2f5d6502cdc4df0e9e0a3b52bf4
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-05-26 04:45:25 -0700

    add: ad-card

---
+++ src/components/AdCard/AdCardProps.ts
@@ -0,0 +1,31 @@
+import { CustomButtonProps } from "../CustomButton/CustomButtonProps";
+
+/**
+ * Props for the AdCard component.
+ */
+export interface AdCardProps {
+  /**
+   * The title of the card.
+   */
+  title?: string;
+
+  /**
+   * The description of the card.
+   */
+  description?: string;
+
+  /**
+   * The URL of the image to be displayed on the card.
+   */
+  image: string;
+
+  /**
+   * The location information to be displayed on the card.
+   */
+  location: string;
+
+  /**
+   * Properties for the show details button.
+   */
+  showDetailsBtn: CustomButtonProps;
+}
