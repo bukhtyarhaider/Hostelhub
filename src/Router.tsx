bd7b882f276d4cedbaf2b3a2bfa6bab367098276
Bukhtyar Haider <bukhtyar.haider1@gmail.com>
2024-07-30 11:00:36 -0700

    My Profile

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,12 +3,10 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import HostelDetails from "./pages/HostelDetails/HostelDetails";
 import { User } from "./types/types";
-import ApplicationSent from "./pages/HostelApplication/ApplicationSent/ApplicationSent";
 import HostelApplication from "./pages/HostelApplication/HostelApplication";
 import MyHostel from "./pages/MyHostel/MyHostel";
 import MyRequests from "./pages/MyRequests/MyRequests";
-import MyHostel from "./pages/MyHostel/MyHostel";
-import MyRequests from "./pages/MyRequests/MyRequests";
+import Profile from "./pages/Profile/Profile";
 
 interface RouterProps {
   authUser?: User;
@@ -37,22 +35,19 @@ const Router: React.FC<RouterProps> = ({
         />
         <Route
           path="/hostel-details/:id"
-          index
           element={<HostelDetails authUser={authUser ?? undefined} />}
         />
         <Route
           path="/hostel-application"
-          index
           element={<HostelApplication authUser={authUser ?? undefined} />}
         />
 
+        <Route path="/my-hostel" element={<MyHostel />} />
+        <Route path="/my-requests" element={<MyRequests />} />
         <Route
-          path="/my-hostel"
-          element={<MyHostel />}
-        />
-        <Route
-          path="/my-hostel"
-          element={<MyHostel />}
+          path="/profile"
+          element={<Profile />}
+          authUser={authUser ?? undefined}
         />
         <Route path="*" element={<Navigate to={"/"} />} />
       </Routes>
