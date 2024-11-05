73993592559f140044d1d335a99581b227f9d58a
Bukhtyar Haider Khan <bukhtyar.haider1@gmail.com>
2024-11-05 10:27:42 +0500

    improve: Documents component

--- src/pages/HostelApplication/Documents/Documents.tsx
+++ src/pages/HostelApplication/Documents/Documents.tsx
@@ -36,8 +36,11 @@ const UploadSection: React.FC<{
 }> = ({ data, formData, setFormData, errors, setErrors }) => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, files } = e.target;
-    setFormData({ ...formData, [name]: files[0] });
-    setErrors({ ...errors, [name]: "" }); // Clear the error for the selected file
+
+    if (files && files.length > 0) {
+      setFormData({ ...formData, [name]: files[0] });
+      setErrors({ ...errors, [name]: "" });
+    }
   };
 
   console.log("errors", errors);
